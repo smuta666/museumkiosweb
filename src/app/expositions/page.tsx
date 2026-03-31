e9a9ad6459848b5e9bc69a0e820a3d4f68b3c89f
Anthony <Anthony@example.com>
2026-03-31 13:03:40 +0300

    fix

--- src/app/expositions/page.tsx
+++ src/app/expositions/page.tsx
@@ -11,7 +11,7 @@ export default async function ExpositionsPage() {
       fullDescription:
         "Исторические интерьеры главного корпуса дворца, сохранившие атмосферу XIX века. Экспозиция знакомит посетителей с оформлением парадных помещений, мебелью, декоративными элементами и атмосферой эпохи.",
       price: "500 ₽",
-      imageUrl: "/uploads/174521141305-2026_04_17-MSR-Alupka.jpg",
+      imageUrl: "/public/static/expositions/exhibition-hall.jpg",
     },
     {
       id: "2",
@@ -20,7 +20,7 @@ export default async function ExpositionsPage() {
       fullDescription:
         "Экспозиция рассказывает о повседневной жизни дворцовой кухни и хозяйственной части комплекса. Посетители могут увидеть предметы быта, кухонную утварь и особенности организации работы службы дворца.",
       price: "400 ₽",
-      imageUrl: "/uploads/17452118836256-2026_04_17-MSR-Alupka.jpg",
+      imageUrl: "/public/static/expositions/kuhnya.jpg",
     },
     {
       id: "3",
@@ -30,7 +30,27 @@ export default async function ExpositionsPage() {
       fullDescription:
         "Экспозиция показывает, как была устроена служебная жизнь дворца, какие обязанности были у персонала и как выглядели жилые помещения служащих.",
       price: "350 ₽",
-      imageUrl: "/uploads/1745217997012-waving-silk-flag-of-russia-illustration-ai-generative-free-photo.jpg",
+      imageUrl: "/public/static/expositions/dvoreckiy.jpg",
+    },
+    {
+      id: "4",
+      title: "ASD",
+      description:
+        "ASD",
+      fullDescription:
+        "ASD",
+      price: "350 ₽",
+      imageUrl: "/public/static/expositions/paradnye-zaly.jpg",
+    },
+    {
+      id: "5",
+      title: "ASD",
+      description:
+        "ASD",
+      fullDescription:
+        "ASD",
+      price: "350 ₽",
+      imageUrl: "/public/static/expositions/gallery.jpg",
     },
   ];
 
