ebc7f65f0736ce30ea7874f395956d361bfdc542
Anthony <Anthony@example.com>
2026-03-31 14:24:28 +0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
           <ScrollToTop />
           <Header />
 
-          <div className="kiosk-container flex-1 py-8 pt-[220px]">
+          <div className="kiosk-container flex-1 px-0 py-8 pt-[260px]">
             <main className="flex h-full flex-col">{children}</main>
           </div>
 
